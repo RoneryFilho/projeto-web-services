e1138a7f54846c6b454642372a5dca84b5dc8131
Ronery Filho <roneryrffilho@gmail.com>
2023-07-21 13:01:56 -0300

    Created the Payment class and it's association with the Order class

--- src/main/java/com/webservices/course/config/TestConfig.java
+++ src/main/java/com/webservices/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.webservices.course.entities.Category;
 import com.webservices.course.entities.Order;
 import com.webservices.course.entities.OrderItem;
+import com.webservices.course.entities.Payment;
 import com.webservices.course.entities.Product;
 import com.webservices.course.entities.User;
 import com.webservices.course.entities.enums.OrderStatus;
@@ -71,14 +72,19 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 }
